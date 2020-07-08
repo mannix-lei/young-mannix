6eb03b658c58c117537bbf62df3a45ebe6620c8f
leimingsheng@zhongbaounion.com <leimingsheng@zhongbaounion.com>
2020-07-08 16:48:45 +0800

    add dockerfile

---
+++ Dockerfile
@@ -0,0 +1,15 @@
+FROM node:14.1-alpine AS builder
+
+WORKDIR /opt/web
+COPY package.json package-lock.json ./
+RUN npm install
+
+ENV PATH="./node_modules/.bin:$PATH"
+
+COPY . ./
+RUN npm run build
+
+FROM nginx:1.17-alpine
+
+COPY ./nginx.config /etc/nginx/nginx.template
+CMD ["/bin/sh", "-c", "envsubst < /etc/nginx/nginx.template > /etc/nginx/conf.d/default.conf && nginx -g 'daemon off;'"]
---
+++ build/precache-manifest.21a81b0c906dc6d1dd98ebab7eebacf4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7144168453678e50b6b0e53774c3e5d4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "23bcdebfe4985affae45",
+    "url": "/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "8c9518d8300241d13530",
+    "url": "/static/css/main.c0078f1e.chunk.css"
+  },
+  {
+    "revision": "23bcdebfe4985affae45",
+    "url": "/static/js/2.9c48b5af.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9c48b5af.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8c9518d8300241d13530",
+    "url": "/static/js/main.7b660769.chunk.js"
+  },
+  {
+    "revision": "de00f035b04bfec510a2",
+    "url": "/static/js/runtime-main.25407149.js"
+  }
+]);
