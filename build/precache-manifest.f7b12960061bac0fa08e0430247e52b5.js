78a1be7dd5a478fda45cd510fc1a10872ebda8b2
leimingsheng@zhongbaounion.com <leimingsheng@zhongbaounion.com>
2020-07-29 16:08:00 +0800

    build

---
+++ build/precache-manifest.f7b12960061bac0fa08e0430247e52b5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "19b3b229cb12496abddd9132ee2862b0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "caec8b8a3390f139c76b",
+    "url": "/static/css/2.9edc93b8.chunk.css"
+  },
+  {
+    "revision": "9f428842fa5faa7f0523",
+    "url": "/static/css/main.7fd76e58.chunk.css"
+  },
+  {
+    "revision": "caec8b8a3390f139c76b",
+    "url": "/static/js/2.4867ed67.chunk.js"
+  },
+  {
+    "revision": "117d65892edc8adcd29d4a0d57b5a80a",
+    "url": "/static/js/2.4867ed67.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9f428842fa5faa7f0523",
+    "url": "/static/js/main.81e771cb.chunk.js"
+  },
+  {
+    "revision": "de00f035b04bfec510a2",
+    "url": "/static/js/runtime-main.25407149.js"
+  },
+  {
+    "revision": "65751f47a85d09932ca49d104ff64c64",
+    "url": "/static/media/WechatIMG1.65751f47.jpeg"
+  },
+  {
+    "revision": "45083ac7bc5cdc52ca2605dc31caf0d4",
+    "url": "/static/media/WechatIMG2.45083ac7.jpeg"
+  }
+]);
