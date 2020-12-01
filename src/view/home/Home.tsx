22626c11664b4272f39ff41cbd6d2fd63e3e0d57
leimingsheng@zhongbaounion.com <leimingsheng@zhongbaounion.com>
2020-12-01 16:43:36 +0800

    latest

--- src/view/home/Home.tsx
+++ src/view/home/Home.tsx
@@ -18,10 +18,10 @@ const Home: FC = () => {
                 <Meta title={<Link to={'/news'}>看会新闻~</Link>} description="Popular news" />
             </Card>
             <Card hoverable className={style.card} cover={<img alt="listImg" src={newsImg} />} onClick={() => history.push('/leetcode')}>
-                <Meta title={<Link to={'/leetcode'}>练习算法~</Link>} description="coding..." />
+                <Meta title={<Link to={'/leetcode'}>看看算法~</Link>} description="coding..." />
             </Card>
             <Card hoverable className={style.card} cover={<img alt="listImg" src={nothing} />} onClick={() => history.push('')}>
-                <Meta title={<Link to={''}>后续板块陆续开启～～你好啊啦啦啦</Link>} description="敬请期待... young mannix" />
+                <Meta title={<Link to={''}>后续板块陆续开启～</Link>} description="敬请期待..." />
             </Card>
         </div>
     );
