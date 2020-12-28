14f15e22559932737c31dac7bd7a920652569bcb
leimingsheng@zhongbaounion.com <leimingsheng@zhongbaounion.com>
2020-12-28 16:12:48 +0800

    随便改点

--- src/view/common/header/Header.tsx
+++ src/view/common/header/Header.tsx
@@ -1,9 +1,9 @@
+import { Avatar, Layout } from 'antd';
 import React, { FC, ReactNode, useEffect, useState } from 'react';
-import { Layout, Avatar } from 'antd';
-import style from './Header.module.scss';
 import { useDispatch, useSelector } from 'react-redux';
-import { CityDispatcher } from '../../../redux/action/city';
 import { RootState } from '../../../redux';
+import { CityDispatcher } from '../../../redux/action/city';
+import style from './Header.module.scss';
 
 const { Header, Content, Footer } = Layout;
 
@@ -19,7 +19,7 @@ const HeaderLayout: FC = (props: IProps) => {
     const dispatcher = useDispatch();
     const rootDispatcher = new CityDispatcher(dispatcher);
     const { cip, cname } = useSelector((state: RootState) => state.city);
-    const [avator, setavator] = useState<number>(Math.floor(Math.random() * (5)) + 1);
+    const [avator, setavator] = useState<number>(Math.floor(Math.random() * 5) + 1);
 
     useEffect(() => {
         rootDispatcher.getLocalCity();
@@ -30,7 +30,9 @@ const HeaderLayout: FC = (props: IProps) => {
                 <div className={style.search}>
                     <div className={style.avator} onClick={() => (window.location.href = '/')}></div>
                 </div>
-                <span>{cname}&nbsp;&nbsp;{cip}</span>
+                <span>
+                    {cname}&nbsp;&nbsp;{cip}
+                </span>
                 {/* <Radio.Group value={locale} onChange={changeLocale}>
                     <Radio.Button key="en" value={enUS}>
                     English
@@ -39,13 +41,13 @@ const HeaderLayout: FC = (props: IProps) => {
                     中文
                     </Radio.Button>
                 </Radio.Group> */}
-                <span onClick={() => setavator(Math.floor(Math.random() * (5)) + 1)}>
+                <span onClick={() => setavator(Math.floor(Math.random() * 5) + 1)}>
                     <Avatar className={style.loginAvatar} src={`http://api.rosysun.cn/sjtx/?type=${avator}`} />
                 </span>
             </Header>
             <Content className={style.content}>
                 {/* <ConfigProvider locale={locale}> */}
-                <div className="site-layout-content">{props.children}</div>
+                <div className='site-layout-content'>{props.children}</div>
                 {/* </ConfigProvider> */}
             </Content>
             <Footer style={{ textAlign: 'center' }}>Young Mannix ©2020 Created by Mannix Lei</Footer>
